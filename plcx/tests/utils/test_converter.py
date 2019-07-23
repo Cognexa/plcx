e2901cff11cd302103221283ef0a33b32191e747
Robert Gildein <gildeinrobert@gmail.com>
2019-07-23 07:55:28 +0200

    add test for bits_to_dict

--- plcx/tests/utils/test_converter.py
+++ plcx/tests/utils/test_converter.py
@@ -2,7 +2,7 @@ import pytest
 
 from bitarray import bitarray
 
-from plcx.utils.converter import read_bits
+from plcx.utils.converter import read_bits, bits_to_dict
 
 
 @pytest.mark.parametrize('bits, type_, expect_value', [
@@ -45,3 +45,26 @@ def test_convert_bits_error(bits, type_, expect_error):
     """
     with pytest.raises(expect_error):
         read_bits(bits, type_)
+
+
+@pytest.mark.parametrize('bits, config, expect_value', [
+    (
+        bitarray('011000010011001000110010001011100011001101'),
+        [(0, 8, 'letter', str), (8, 40, 'number', float), (40, 41, 'bool1', bool), (41, 42, 'bool2', bool)],
+        {'letter': 'a', 'number': 22.3, 'bool1': False, 'bool2': True}
+    ),
+    (
+        bitarray('0001100010010111000110010001101011'),
+        [(0, 1, 'bool1', bool), (1, 25, 'float', float), (25, 33, 'int', int), (33, 34, 'bool2', bool)],
+        {'bool1': False, 'float': 1.2, 'int': 5, 'bool2': True}
+    )
+])
+def test_convert_bits_to_dict(bits, config, expect_value):
+    """
+    Test of convert bits array to dict/json from configuration.
+
+    :param bits: bits array
+    :param config: configuration of converting
+    :param expect_value: expected values in dictionary
+    """
+    assert bits_to_dict(bits, config) == expect_value
