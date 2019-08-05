9e8cdd7ff235fb48b58ba0650b6ef18cfa01210d
Robert Gildein <gildeinrobert@gmail.com>
2019-08-05 15:27:40 +0200

    add reader class object

---
+++ plcx/bag/reader.py
@@ -0,0 +1,42 @@
+import logging
+import struct
+
+from dataclasses import dataclass
+from typing import Any, Dict, List, Tuple
+
+from plcx.bag.unpack import bytes_to_list, bytes_to_dict
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Reader:
+    tag: Tuple[str, Any]
+    message_format: List[Tuple[str, str]]
+    byte_order: str = '@'
+
+    def is_readable(self, message: bytes) -> bool:
+        """
+        Test if reader could read message.
+
+        :param message: bytes message
+        :return: bool value
+        """
+        format_, exp_value = self.tag
+        try:
+            value = bytes_to_list(message, format_, self.byte_order)[0]
+        except struct.error:
+            logger.debug(f'Reader try to read `{message}`.')
+            return False
+        else:
+            logger.debug(f'Reader read tag value `{value}` and expected `{exp_value}`.')
+            return value == exp_value
+
+    def read(self, message: bytes) -> Dict[str, Any]:
+        """
+        Unpack message to dictionary.
+
+        :param message: bytes message
+        :return: dictionary with parameters
+        """
+        return bytes_to_dict(message, self.message_format, self.byte_order)
