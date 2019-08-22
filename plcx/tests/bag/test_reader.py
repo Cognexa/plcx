eba2cd4ffaf7beff26c9f47569fbb321759e77cc
Robert Gildein <gildeinrobert@gmail.com>
2019-08-22 14:42:34 +0200

    fix reader tag and arguments

--- plcx/tests/bag/test_reader.py
+++ plcx/tests/bag/test_reader.py
@@ -8,20 +8,20 @@ from plcx.utils.boolean import list_to_byte
 @pytest.mark.parametrize('msg, tag, arguments, byte_order, exp_value', [
     (
         struct.pack('=B3s?', 1, b'abc', False),  # message
-        ('B4x', 1),  # message tag
-        [(None, 'x'), ('t1', '3s'), ('t2', '?')],  # message format
+        ('B', 1),  # message tag
+        [('t1', '3s'), ('t2', '?')],  # message format
         '=',
         {'t1': b'abc', 't2': False}  # expected value
     ), (
-        struct.pack('=''s''2s''i''i''?''c', b'p', b'ab', 5, 4, True, list_to_byte([True, False, True])),
-        ('x2s10x', b'ab'),
-        [(None, 'x'), (None, '2x'), ('i1', 'i'), ('i2', 'i'), ('b', '?'), ('lb', '#')],
+        struct.pack('=''2s''i''i''?''c', b'ab', 5, 4, True, list_to_byte([True, False, True])),
+        ('2s', b'ab'),
+        [('i1', 'i'), ('i2', 'i'), ('b', '?'), ('lb', '#')],
         '=',
         {'i1': 5, 'i2': 4, 'b': True, 'lb': [True, False, True, False, False, False, False, False]}
     ), (
-        struct.pack('=2sx2s2i', b'tt', b'ab', 5, 4),
-        ('2s11x', b'tt'),
-        [(None, '3x'), ('text', '2s'), ('integers', '2i')],
+        struct.pack('=c2sx2s2i', b'x', b'tt', b'ab', 5, 4),
+        ('x2s', b'tt'),
+        [(None, 'x'), ('text', '2s'), ('integers', '2i')],
         '=',
         {'text': b'ab', 'integers': [5, 4]}
     )
@@ -38,27 +38,26 @@ def test_reader(msg, tag, arguments, byte_order, exp_value):
     """
     reader = Reader(tag, arguments, byte_order)
 
-    print(reader.read(msg))
-
     assert reader.is_readable(msg)
     assert reader.read(msg) == exp_value
 
 
-@pytest.mark.parametrize('msg, tag, byte_order, is_readable', [
-    (struct.pack('=''s''c', b'a', list_to_byte([True, False, True])), ('sx', b'a'), '=', True),
-    (struct.pack('=''s''c', b'a', list_to_byte([True, False, True])), ('sx', b'b'), '=', False),
-    (struct.pack('=''2s''c', b'ab', list_to_byte([True, False, True])), ('sx', b'a'), '=', False),
-    (struct.pack('=''2s''c', b'ab', list_to_byte([True, False, True])), ('s2x', b'a'), '=', True)
+@pytest.mark.parametrize('msg, tag, arguments, byte_order, is_readable', [
+    (struct.pack('=''s''c', b'a', b't'), ('s', b'a'), [(None, 'x')], '=', True),
+    (struct.pack('=''s''c', b'a', b't'), ('s', b'b'), [(None, 'x')], '=', False),
+    (struct.pack('=''2s''c', b'ab', b't'), ('s', b'a'), [(None, '2x')], '=', True),
+    (struct.pack('=''2s''c', b'ab', b't'), ('2s', b'ab'), [(None, 'x')], '=', True)
 ])
-def test_reader_message_not_readable(msg, tag, byte_order, is_readable):
+def test_reader_message_not_readable(msg, tag, arguments, byte_order, is_readable):
     """
     Test Reader tes message readability.
 
     :param msg: bytes message
     :param tag: message tag
+    :param arguments: list of parameters, define as (<name>, <format>)
     :param byte_order: indicate the byte order
     :param is_readable: bool if message could be readable
     """
-    reader = Reader(tag, [], '=')
+    reader = Reader(tag, arguments, '=')
 
     assert reader.is_readable(msg) == is_readable
--- plcx/tests/bag/test_writer.py
+++ plcx/tests/bag/test_writer.py
@@ -4,27 +4,29 @@ import struct
 from plcx.bag.writer import Writer
 
 
-@pytest.mark.parametrize('kwargs, tag, arguments, byte_order, exp_value', [
+@pytest.mark.parametrize('kwargs, tag, arguments, byte_order, exp_value, exp_size', [
     (
         {'a': 5, 'b': 3, 'c': 4},
         ('c', b'a'),
         [('a', 'i'), ('b', 'i'), ('c', 'i')],
         '@',
-        b'a\x00\x00\x00\x05\x00\x00\x00\x03\x00\x00\x00\x04\x00\x00\x00'
+        b'a\x00\x00\x00\x05\x00\x00\x00\x03\x00\x00\x00\x04\x00\x00\x00',
+        16
     ),
     (
         {'a': 5, 'b': 3, 'c': 4},
         ('c', b'a'),
         [('a', 'i'), ('b', 'i'), ('c', 'i')],
         '=',
-        b'a\x05\x00\x00\x00\x03\x00\x00\x00\x04\x00\x00\x00'
+        b'a\x05\x00\x00\x00\x03\x00\x00\x00\x04\x00\x00\x00',
+        13
     ),
-    ({'a': 5, 'b': b'a'}, ('i', 101), [('a', 'i'), ('b', 's')], '=', b'e\x00\x00\x00\x05\x00\x00\x00a'),
-    ({'a': 5, 'b': b'a'}, ('i', 101), [('a', 'i'), ('b', 's')], '@', b'e\x00\x00\x00\x05\x00\x00\x00a'),
-    ({'a': 5}, ('c', b'S'), [(None, 'x'), ('a', 'i')], '@', b'S\x00\x00\x00\x05\x00\x00\x00'),
-    ({'a': [1, 2]}, ('c', b'S'), [(None, 'x'), ('a', '2i')], '@', b'S\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00'),
+    ({'a': 5, 'b': b'a'}, ('i', 101), [('a', 'i'), ('b', 's')], '=', b'e\x00\x00\x00\x05\x00\x00\x00a', 9),
+    ({'a': 5, 'b': b'a'}, ('i', 101), [('a', 'i'), ('b', 's')], '@', b'e\x00\x00\x00\x05\x00\x00\x00a', 9),
+    ({'a': 5}, ('c', b'S'), [(None, 'x'), ('a', 'i')], '@', b'S\x00\x00\x00\x05\x00\x00\x00', 8),
+    ({'a': [1, 2]}, ('c', b'S'), [(None, 'x'), ('a', '2i')], '@', b'S\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00', 12),
 ])
-def test_write(kwargs, tag, arguments, byte_order, exp_value):
+def test_write(kwargs, tag, arguments, byte_order, exp_value, exp_size):
     """
     Test base behavior of plcx.bag.writer.Writer.
 
@@ -33,10 +35,14 @@ def test_write(kwargs, tag, arguments, byte_order, exp_value):
     :param arguments: list of parameters, define as (<name>, <format>)
     :param byte_order: indicate the byte order
     :param exp_value: expected value
+    :param exp_size: expected message size
     """
     writer = Writer(tag, arguments, byte_order)
 
-    assert writer.write(**kwargs) == exp_value
+    message = writer.write(**kwargs)
+
+    assert message == exp_value
+    assert len(message) == exp_size
 
 
 @pytest.mark.parametrize('kwargs, tag, arguments, byte_order, error', [
