8024eb8fcd1f4bbe21942ca53fab3353818db667
Robert Gildein <gildeinrobert@gmail.com>
2019-10-14 07:40:45 +0200

    use big with MSB

--- plcx/tests/utils/test_boolean.py
+++ plcx/tests/utils/test_boolean.py
@@ -6,8 +6,8 @@ from plcx.utils.boolean import byte_to_booleans, boolean_to_byte
 @pytest.mark.parametrize("one_byte, bit_order, exp", [
     (int("00100100", 2).to_bytes(1, "little"), "LSB", [False, False, True, False, False, True, False, False]),
     (int("10100001", 2).to_bytes(1, "little"), "LSB", [True, False, True, False, False, False, False, True]),
-    (int("10100001", 2).to_bytes(1, "little"), "MSB", [True, False, False, False, False, True, False, True]),
-    (int("10000011", 2).to_bytes(1, "little"), "MSB", [True, True, False, False, False, False, False, True]),
+    (int("10000101", 2).to_bytes(1, "big"), "MSB", [True, False, True, False, False, False, False, True]),
+    (int("11000001", 2).to_bytes(1, "big"), "MSB", [True, False, False, False, False, False, True, True]),
 ])
 def test_byte_to_booleans(one_byte, bit_order, exp):
     """Test converting one byte to list of boolean."""
@@ -18,8 +18,8 @@ def test_byte_to_booleans(one_byte, bit_order, exp):
     ([True, False, True, False, False, True, False, False], "LSB", int("10100100", 2).to_bytes(1, "little")),
     ([True, False, True], "LSB", int("10100000", 2).to_bytes(1, "little")),
     (([True], [True], ), "LSB", int("1000000010000000", 2).to_bytes(2, "little")),
-    (([True], [True], ), "MSB", int("0000000100000001", 2).to_bytes(2, "little")),
-    ([True, False, True], "MSB", int("00000101", 2).to_bytes(1, "little")),
+    ((7*[False] + [True], 7*[False] + [True], ), "MSB", int("1000000010000000", 2).to_bytes(2, "big")),
+    ([False, False, False, False, False, True, False, True], "MSB", int("10100000", 2).to_bytes(1, "big")),
 ])
 def test_list_to_byte(boolean_list, bit_order, exp):
     """Test converting list of boolean to one byte,"""
